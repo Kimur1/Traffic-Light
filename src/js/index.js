4a3de3671e0a46139ef7aca969129c70e8718492
Kimberly Murillo Castro <kimur53@gmail.com>
2021-03-17 11:35:55 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Home } from "./component/home";
 
 //render your react application
-ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
